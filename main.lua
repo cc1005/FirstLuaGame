ff2f9061691c7337aaa2d6bdfa9721b76f56f53d
Conor Campbell <conorkampbell@hotmail.com>
2022-10-05 13:30:28 +0100

    Added a basic animation and first variables to test

--- main.lua
+++ main.lua
@@ -1,13 +1,33 @@
 _G.love = require("love")
 
 function love.load()
+    love.graphics.setBackgroundColor(0.5, 0.5, 1)
 
+    _G.pacman = {}
+    pacman.x = 700
+    pacman.y = 400
+    pacman.eat = false
+
+    _G.food_x = 850
 end
 
 function love.update(dt)
+    pacman.x = pacman.x + 1
+    
+    if pacman.x == food_x + 20 then
+        pacman.eat = true
+    end
 
 end
 
 function love.draw()
-    love.graphics.rectangle("fill", 50, 50, 50, 50)
+    
+    if not pacman.eat then
+        love.graphics.setColor(1, 0, 0)
+        love.graphics.rectangle("fill", food_x, 370, 50, 50)    
+    end
+
+
+    love.graphics.setColor(1, 0.8, 0.0)
+    love.graphics.arc("fill", pacman.x, pacman.y, 100, 0.5, 5.5)
 end
