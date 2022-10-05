dc826ceb5154cbe168bdd3a9ea7f5c9dfa890b59
Conor Campbell <conorkampbell@hotmail.com>
2022-10-05 13:05:28 +0100

    Updated the config file and also began drawing some shapes to the screen

--- conf.lua
+++ conf.lua
@@ -1,3 +1,6 @@
 function love.conf(t)
-    t.identity = "data/saves"
+    t.identity = "data/saves" -- assign a save file
+    t.window.width = 1024
+    t.window.height = 768
+    t.window.title = "Conor's First Lua Game"
 end
--- main.lua
+++ main.lua
@@ -1,17 +1,13 @@
 _G.love = require("love")
 
 function love.load()
-    _G.number = 0
+
 end
 
 function love.update(dt)
-    number = number + 1
-    if number == 60 then
-        number = 0
-    end
 
 end
 
 function love.draw()
-    love.graphics.print("Hello World " .. number, 400, 400)
+    love.graphics.rectangle("fill", 50, 50, 50, 50)
 end
